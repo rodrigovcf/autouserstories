6e2cfb5eea23efad5165d537843f0819cd1c0c08
rodrigo.frade <rodrigo.frade@bradescoseguros.com.br>
2025-03-31 10:26:45 -0300

    Iniciando do projeto

---
+++ src/main/java/com/autous/autouserstories/AutouserstoriesApplication.java
@@ -0,0 +1,13 @@
+package com.autous.autouserstories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutouserstoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutouserstoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autous/autouserstories/AutouserstoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autous.autouserstories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutouserstoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
